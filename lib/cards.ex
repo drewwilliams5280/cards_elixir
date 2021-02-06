e1d90e6e530d8132e08bbba7a7aff8259c12d3cd
Drew Williams <drewwilliams5280@gmail.com>
2021-02-06 16:22:37 -0700

    Deck create method complete

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,19 @@
 defmodule Cards do
-
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
+              "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
